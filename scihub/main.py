0e122a025e5c25ca3c5105f30328ccd92cc29844
Gorka Abad <gorkaabad@gmail.com>
2021-06-22 13:14:02 +0200

    Add number of papers to download

--- scihub/main.py
+++ scihub/main.py
@@ -13,7 +13,7 @@ import string
 baseUrl = 'https://scholar.google.es/scholar?hl=es&as_sdt=0%2C5&q=allintitle%3A+'
 finalUrl = '&btnG=&oq='
 
-def searchScholar(keywords):
+def searchScholar(keywords, n):
     hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
@@ -26,25 +26,34 @@ def searchScholar(keywords):
     res = urllib.request.urlopen(req)
     if res.status == 200:
         soup = BeautifulSoup(res.read(), 'lxml')
-
+        count = 1
         for paper in soup.find_all('div',{'class':'gs_r gs_or gs_scl'}):
+                if count > n[0]: break      
+ 
                 try:
-                    
-                    print("----------------------")
+                    print('[*] Donwloading... {}/{}.'.format(count,n[0]))
                     url = paper.a.get('href')
                     filename = paper.b.string
                     rand = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
-                    filename = filename + rand
-                    print('URL: {}'.format(url))
-                    print('Filename: {}'.format(filename))
-                    if None != url:
-                            out = '{}.pdf'.format(filename)
-                            filename = Path(out)
-                            res = requests.get(url,verify=False)
-                            filename.write_bytes(res.content)
+                    getPDF(filename,url)
                 except Exception as e:
                     print('[!] An error ocurred: {}'.format(e))
 
+                count += 1
+
+def getPDF(filename, url):
+
+    rand = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
+    filename = filename + rand
+
+    if None != url:
+        out = '{}.pdf'.format(filename)
+        filename = Path(out)
+        print(filename)
+        res = requests.get(url,verify=False)
+        filename.write_bytes(res.content)
+        
+        print(' [*] Downloaded successfully.')
 
 
 def sciBulk(input_path):
@@ -70,37 +79,36 @@ def sciBulk(input_path):
                 if key in paper:
                     # If it is avaliable at Arxiv, download from there
                     if 'arxiv' in paper[key]:
-
                         url = paper[key].replace('abs', 'pdf')
                         url = '{}.pdf'.format(url)
 
-                        filename = Path(out)
-                        res = requests.get(url)
-                        filename.write_bytes(res.content)
+                        getPDF(paper['title'],url)
+                        break
 
                     else:
                         res = sh.download(paper[key], path=out)
 
-                    if 'err' not in res:
-                        print('     [*] Downloaded successfully.')
-                        break
-                    else:
-                        print(
-                            '     [Info] Triying to donwload with another method.')
+                        if 'err' not in res:
+                            print(' [*] Downloaded successfully.')
+                            break
+                        else:
+                            print(' [Info] Triying to download with another method.')
 
             count += 1
 
     except Exception as e:
         print('[!] An error ocurred while downloading you file. Triying next')
         print(e)
-
+    
 
 def init():
     parser = argparse.ArgumentParser()
     parser.add_argument('-dblp', help='Supply a json file containing the papers form dblp.',
-                        metavar='Path to the json file.', type=Path)
+                        metavar='Path.', type=Path)
     parser.add_argument('-scholar', help='Supply keywords that must be used to donwload papers from scholar.',
                         metavar='Keyword', type=str, nargs='*')
+    
+    parser.add_argument('-n', help='Number of papers to download. Use with -scholar.', type=int, nargs=1,default=1)
     args = parser.parse_args()
     return args
 
@@ -110,5 +118,5 @@ if __name__ == "__main__":
     args = init()
     if args.dblp:
         sciBulk(args.dblp)
-    else:
-        searchScholar(args.scholar)
+    elif args.scholar and args.n:
+        searchScholar(args.scholar,args.n)
